b32004ff32d7295e72685b046c9b356bfc972372
Gaetano Salvatore Falco <gaetanosalvatore.falco@protonmail.com>
2022-11-17 13:45:44 +0100

    Update HW_1_EX_2.py

--- HW1/HW_1_EX_2.py
+++ HW1/HW_1_EX_2.py
@@ -29,31 +29,30 @@ retention_period_1d=86400000
 retention_period_30d=retention_period_1d*30
 
 
-
 # Create a TimeSeries with chunk size 128 bytes
 # By default, compression is enabled
 try:
-    redis_client.ts().create('0xb0a46095b5ff:battery', chunk_size=128, retention=retention_period_1d)
-    redis_client.ts().create('0xb0a46095b5ff:power', chunk_size=128, retention=retention_period_1d)
-    redis_client.ts().create('0xb0a46095b5ff:battery_min', chunk_size=128, retention=retention_period_30d)
-    redis_client.ts().createrule('0xb0a46095b5ff:battery','0xb0a46095b5ff:battery_min',min,60000)
-    redis_client.ts().create('0xb0a46095b5ff:battery_max', chunk_size=128, retention=retention_period_30d)
-    redis_client.ts().createrule('0xb0a46095b5ff:battery','0xb0a46095b5ff:battery_max',max,60000)
-    redis_client.ts().create('0xb0a46095b5ff:battery_avg', chunk_size=128, retention=retention_period_30d)
-    redis_client.ts().createrule('0xb0a46095b5ff:battery','0xb0a46095b5ff:battery_avg',avg,30000)
-    redis_client.ts().create('0xb0a46095b5ff:power_avg', chunk_size=128, retention=retention_period_30d)
-    redis_client.ts().createrule('0xb0a46095b5ff:power','0xb0a46095b5ff:power_avg',avg,30000)
-    redis_client.ts().create('0xb0a46095b5ff:battery_uncompressed', chunk_size=128, retention=retention_period_1d)
-    redis_client.ts().create('0xb0a46095b5ff:power_uncompressed', chunk_size=128, retention=retention_period_1d)
-    redis_client.ts().create('0xb0a46095b5ff:battery_min_uncompressed', chunk_size=128, retention=retention_period_30d)
+    redis_client.ts().create('{mac_address}:battery', chunk_size=128, retention=retention_period_1d)
+    redis_client.ts().create('{mac_address}:power', chunk_size=128, retention=retention_period_1d)
+    redis_client.ts().create('{mac_address}:battery_min', chunk_size=128, retention=retention_period_30d)
+    redis_client.ts().createrule('{mac_address}:battery','{mac_address}:battery_min',min,60000)
+    redis_client.ts().create('{mac_address}:battery_max', chunk_size=128, retention=retention_period_30d)
+    redis_client.ts().createrule('{mac_address}:battery','{mac_address}:battery_max',max,60000)
+    redis_client.ts().create('{mac_address}:battery_avg', chunk_size=128, retention=retention_period_30d)
+    redis_client.ts().createrule('{mac_address}:battery','{mac_address}:battery_avg',avg,30000)
+    redis_client.ts().create('{mac_address}:power_avg', chunk_size=128, retention=retention_period_30d)
+    redis_client.ts().createrule('{mac_address}:power','{mac_address}:power_avg',avg,30000)
+    redis_client.ts().create('{mac_address}:battery_uncompressed', chunk_size=128, retention=retention_period_1d)
+    redis_client.ts().create('{mac_address}:power_uncompressed', chunk_size=128, retention=retention_period_1d)
+    redis_client.ts().create('{mac_address}:battery_min_uncompressed', chunk_size=128, retention=retention_period_30d)
 
-    redis_client.ts().createrule('0xb0a46095b5ff:battery_uncompressed','0xb0a46095b5ff:battery_min_uncompressed',min,60000)
-    redis_client.ts().create('0xb0a46095b5ff:battery_max_uncompressed', chunk_size=128, retention=retention_period_30d)
-    redis_client.ts().createrule('0xb0a46095b5ff:battery_uncompressed','0xb0a46095b5ff:battery_max_uncompressed',max,60000)
-    redis_client.ts().create('0xb0a46095b5ff:battery_avg_uncompressed', chunk_size=128, retention=retention_period_30d)
-    redis_client.ts().createrule('0xb0a46095b5ff:battery_uncompressed','0xb0a46095b5ff:battery_avg_uncompressed',avg,30000)
-    redis_client.ts().create('0xb0a46095b5ff:power_avg_uncompressed', chunk_size=128, retention=retention_period_30d)
-    redis_client.ts().createrule('0xb0a46095b5ff:power_uncompressed','0xb0a46095b5ff:power_avg_uncompressed',avg,30000)
+    redis_client.ts().createrule('{mac_address}:battery_uncompressed','{mac_address}:battery_min_uncompressed',min,60000)
+    redis_client.ts().create('{mac_address}:battery_max_uncompressed', chunk_size=128, retention=retention_period_30d)
+    redis_client.ts().createrule('{mac_address}:battery_uncompressed','{mac_address}:battery_max_uncompressed',max,60000)
+    redis_client.ts().create('{mac_address}:battery_avg_uncompressed', chunk_size=128, retention=retention_period_30d)
+    redis_client.ts().createrule('{mac_address}:battery_uncompressed','{mac_address}:battery_avg_uncompressed',avg,30000)
+    redis_client.ts().create('{mac_address}:power_avg_uncompressed', chunk_size=128, retention=retention_period_30d)
+    redis_client.ts().createrule('{mac_address}:power_uncompressed','{mac_address}:power_avg_uncompressed',avg,30000)
     # redis_client.ts().create('temperature', chunk_size=128)
 except redis.ResponseError:
     print("Cannot create some TimeSeries")
@@ -69,10 +68,10 @@ while True:
     timestamp_ms = int(time() * 1000)
     battery_level = psutil.sensors_battery().percent
     power_plugged = int(psutil.sensors_battery().power_plugged)
-    redis_client.ts().add('0xb0a46095b5ff:battery', timestamp_ms, battery_level)
-    redis_client.ts().add('0xb0a46095b5ff:power', timestamp_ms, power_plugged)
-    redis_client.ts().add('0xb0a46095b5ff:battery_uncompressed', timestamp_ms, battery_level)
-    redis_client.ts().add('0xb0a46095b5ff:power_uncompressed', timestamp_ms, power_plugged)
+    redis_client.ts().add('{mac_address}:battery', timestamp_ms, battery_level)
+    redis_client.ts().add('{mac_address}:power', timestamp_ms, power_plugged)
+    redis_client.ts().add('{mac_address}:battery_uncompressed', timestamp_ms, battery_level)
+    redis_client.ts().add('{mac_address}:power_uncompressed', timestamp_ms, power_plugged)
 
     formatted_datetime = datetime.fromtimestamp(time() ).strftime('%Y-%m-%d %H:%M:%S.%f')
     print(f'{formatted_datetime} - {mac_address}:battery = {battery_level}')
