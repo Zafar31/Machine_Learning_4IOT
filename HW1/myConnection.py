bc5db790759eb3a596486838092059ada5cc044d
Gulisidan <gulisidan.kuerxi@protonmail.com>
2022-11-17 14:49:21 +0100

    Update myConnection.py

--- HW1/myConnection.py
+++ HW1/myConnection.py
@@ -3,6 +3,6 @@
 def getMyConnectionDetails():
     serverIP    = 127.0.0.1
     port        = 10828
-    username    = "default"
+    username    = "defaull"
     password    = "getyourown"
     return serverIP,port,username,password
