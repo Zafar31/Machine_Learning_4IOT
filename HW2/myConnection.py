05321259c7e7e2e3a07c52dd9614cdde8af212a6
Zafar31 <38731131+Zafar31@users.noreply.github.com>
2022-12-19 16:22:04 +0100

    default my connection file

---
+++ HW2/myConnection.py
@@ -0,0 +1,8 @@
+
+
+def getMyConnectionDetails():
+    serverIP    = 127.0.0.1
+    port        = 10828
+    username    = "default"
+    password    = "getyourown"
+    return serverIP,port,username,password
