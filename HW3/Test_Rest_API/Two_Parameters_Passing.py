eb80a559e7143e7ce035042b202c3838fc39e197
Gaetano Salvatore Falco <gaetanosalvatore.falco@protonmail.com>
2023-01-17 11:43:35 +0100

    Two parameters using **query

--- HW3/Test_Rest_API/Two_Parameters_Passing.py
+++ HW3/Test_Rest_API/Two_Parameters_Passing.py
@@ -2,14 +2,16 @@ import cherrypy
 
 class GreetingGenerator(object):
     @cherrypy.expose
-    def index(self, name=None, surname=None):
+    def GET(self, *path, **query):
+        name = query.get('name')
+        surname = query.get('surname')
         if name and surname:
             return "Hi " + name + " " + surname + "!"
         else:
             return '''
                 <html>
                     <body>
-                        <form method="get" action="index">
+                        <form method="get" action="">
                             Name: <input type="text" name="name" />
                             Surname: <input type="text" name="surname" />
                             <input type="submit" value="Greet me!" />
@@ -17,5 +19,6 @@ class GreetingGenerator(object):
                     </body>
                 </html>
             '''
-
-cherrypy.quickstart(GreetingGenerator())
+    index = GET
+    
+cherrypy.quickstart(GreetingGenerator(), '/')
